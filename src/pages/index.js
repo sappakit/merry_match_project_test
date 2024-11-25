764c1bcdc7aea66d9efbd4f7a06910a7174a44e3
Eisss20 <pajenan.j@gmail.com>
2024-11-25 22:00:19 +0700

    chore: add DaisyUI plugin to Tailwind configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <div>
       <h1 className="font-nunito text-8xl text-primary-400">test</h1>
+      <input type="text" placeholder="Test UI" className="input input-bordered w-full bg-red-100 text-red-600 placeholder-red-400 max-w-xs"/>
+
     </div>
+
+    
   );
 }
